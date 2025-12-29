6664967bdce8aec29b8c004273b4ad88ad469a83
RamyaShankarkumar <40883090+RamyaShankarkumar@users.noreply.github.com>
2025-12-29 13:22:20 -0500

    Create driver_factory.py

---
+++ utils/driver_factory.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def get_driver():
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--window-size=1920,1080")
+
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=options
+    )
+
+    driver.implicitly_wait(0)
+    return driver
